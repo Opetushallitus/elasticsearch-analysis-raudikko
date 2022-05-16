f02a29bee8dd793e97e269d5efac1df4842c4850
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-05-16 16:27:02 +0300

    ES 7.17.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val baseVersion = "0.1"
-val elasticsearchVersion = "7.17.2"
+val elasticsearchVersion = "7.17.3"
 
 group = "fi.evident.elasticsearch"
 version = "$baseVersion-es$elasticsearchVersion"
