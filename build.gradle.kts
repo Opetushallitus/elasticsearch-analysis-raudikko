685d3562db5a67ef5159b5dc324d388400cf9664
August Kilponen <august.kilponen@gmail.com>
2024-10-15 09:12:52 +0300

    Päivitetty ElasticSearch-versioon 8.15.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val baseVersion = System.getProperty("baseVersion") ?: "0.1"
-val elasticsearchVersion = System.getProperty("elasticsearchVersion") ?: "8.5.2"
+val elasticsearchVersion = System.getProperty("elasticsearchVersion") ?: "8.15.0"
 
 group = "fi.evident.elasticsearch"
 version = "$baseVersion-es$elasticsearchVersion"
